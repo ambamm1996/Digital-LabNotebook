d99a41745f36e49228faf2803a9071ee56bcbc55
all919927 <all919927@email.ccbcmd.edu>
2026-04-21 16:33:04 +0000

    begin

--- git.R
+++ git.R
@@ -1,6 +1,6 @@
 usethis::use_git_config(
-  user.name = "ccbcanpstudent",
-  user.email = "ccbcanpstudent@gmail.com",
+  user.name = "all919927",
+  user.email = "all919927@email.ccbcmd.edu",
 )
 
 usethis::create_github_token()
